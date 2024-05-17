4a6747d29b74eeee3b3cc319f070559ef42788bf
est-gonzalezr <74991415+est-gonzalezr@users.noreply.github.com>
2024-05-17 18:18:22 -0500

    Working project

--- distribution/lib/Treedoc.ex
+++ distribution/lib/Treedoc.ex
@@ -5,10 +5,9 @@ defmodule Treedoc do
     %Treedoc{}
   end
 
-  def insert(%Treedoc{tree: tree} = doc, msg) do
-    id = generate_id()
-    updated_tree = Map.put(tree, id, %{msg: msg, deleted: false})
-    {%Treedoc{doc | tree: updated_tree}, id}
+  def insert(%Treedoc{tree: tree} = doc, {pos_id, msg}) do
+    updated_tree = Map.put(tree, pos_id, %{msg: msg, deleted: false})
+    %Treedoc{doc | tree: updated_tree}
   end
 
   def delete(%Treedoc{tree: tree} = doc, pos_id) do
@@ -17,14 +16,16 @@ defmodule Treedoc do
   end
 
   def print_all_messages(%Treedoc{tree: tree}) do
-    Enum.each(tree, fn {id, %{msg: msg, deleted: deleted}} ->
+    Enum.sort(tree, fn {id1, _}, {id2, _} -> id1 <= id2 end)
+    |> Enum.each(fn {id, %{msg: msg, deleted: deleted}} ->
       status = if deleted, do: "(deleted)", else: ""
       IO.puts("#{id}: #{msg} #{status}")
     end)
   end
 
   def print_actual(%Treedoc{tree: tree}) do
-    Enum.each(tree, fn {id, %{msg: msg, deleted: deleted}} ->
+    Enum.sort(tree, fn {id1, _}, {id2, _} -> id1 <= id2 end)
+    |> Enum.each(fn {id, %{msg: msg, deleted: deleted}} ->
       unless deleted, do: IO.puts("#{id}: #{msg}")
     end)
   end
---
+++ distribution/lib/chat.ex
@@ -0,0 +1,89 @@
+defmodule Chat do
+  def insert({pos_id, str}) do
+    IO.puts("pos_id: #{pos_id}, str: #{str}")
+    Agent.update(:treedoc, &Treedoc.insert(&1, {pos_id, str}))
+  end
+
+  def delete(pos_id) do
+    IO.puts("pos_id: #{pos_id}")
+    Agent.update(:treedoc, &Treedoc.delete(&1, pos_id))
+  end
+
+  def print_all_messages() do
+    Agent.get(:treedoc, &Treedoc.print_all_messages(&1))
+  end
+
+  def print_actual() do
+    Agent.get(:treedoc, &Treedoc.print_actual(&1))
+  end
+
+  def send_message(recipient, message, :insert) do
+    spawn_task(__MODULE__, :insert, recipient, [message])
+  end
+
+  def send_message(recipient, message, :delete) do
+    spawn_task(__MODULE__, :delete, recipient, [message])
+  end
+
+  def spawn_task(module, fun, recipient, args) do
+    recipient
+    |> remote_supervisor()
+    |> Task.Supervisor.async(module, fun, args)
+    |> Task.await()
+  end
+
+  defp remote_supervisor(recipient) do
+    {Chat.TaskSupervisor, recipient}
+  end
+
+  def option_loop() do
+    IO.puts("\n1. Send message")
+    IO.puts("2. Delete message")
+    IO.puts("3. Print all messages")
+    IO.puts("4. Print actual messages")
+    IO.puts("5. Exit")
+
+    case IO.gets("Choose an option: ") |> String.trim() do
+      "1" ->
+        msg = IO.gets("Enter message: ") |> String.trim()
+        element = {:os.system_time(), msg}
+        Agent.update(:treedoc, &Treedoc.insert(&1, element))
+
+        Enum.each(Node.list(), fn node ->
+          send_message(node, element, :insert)
+        end)
+
+      "2" ->
+        pos_id =
+          IO.gets("Enter message id: ")
+          |> String.trim()
+          |> String.to_integer()
+
+        Agent.update(:treedoc, &Treedoc.delete(&1, pos_id))
+
+        Enum.each(Node.list(), fn node ->
+          send_message(node, pos_id, :delete)
+        end)
+
+      "3" ->
+        print_all_messages()
+
+      "4" ->
+        print_actual()
+
+      "5" ->
+        IO.puts("Exiting...")
+        exit(:normal)
+
+      _ ->
+        IO.puts("Invalid option")
+    end
+
+    option_loop()
+  end
+
+  def main do
+    Agent.start_link(fn -> Treedoc.new() end, name: :treedoc)
+    option_loop()
+  end
+end
